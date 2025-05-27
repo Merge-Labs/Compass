cc31323e9f9e8d87bdc314deee1b33d861b4d739
Manasseh Gitau <kimmanasseh14@gmail.com>
2025-05-28 00:10:33 +0300

    feat: add login page with protected routes Co-authored-by: Wahome Jeremy <wahome@nisria.co>

--- nisria-backend/compass/settings.py
+++ nisria-backend/compass/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
     'django_filters',
     'django_celery_beat',
